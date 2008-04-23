e400db498837cd155f8c36769ceffe3cd4189833
milk2cows <milk2cows@b8d21008-e54a-0410-bd0d-c175464d291e>
2008-04-23 09:11:30 +0000

    add reset_still to handler master and slave spider autorun.

--- trunk/ wormstory/lib/jdriver.py
+++ trunk/ wormstory/lib/jdriver.py
@@ -8,23 +8,25 @@ class jdriver(jioe):
 		self.MASTER_X = 0
 		self.MASTER_Y = 1
 		
-		# add a still logic
-		self.SLAVE_Y_still = 0
-		self.SLAVE_X_still = 0
+		self.SLAVE_X_still = self.SLAVE_Y_still = 0
 		
-		jioe.__init__(self) 
+		jioe.__init__(self)
 		
-	def local_spider_move(self, rect, speed):		
-		if self.all_axis[self.SLAVE_X]  and  self.all_axis[self.SLAVE_Y]: 
-			self.SLAVE_X_still = self.all_axis[self.SLAVE_X]
-			self.SLAVE_Y_still = self.all_axis[self.SLAVE_Y]
-		elif self.all_axis[self.SLAVE_X] and not self.all_axis[self.SLAVE_Y]:
-			self.SLAVE_X_still = self.all_axis[self.SLAVE_X]
-			self.SLAVE_Y_still = 0
-		elif not self.all_axis[self.SLAVE_X] and self.all_axis[self.SLAVE_Y]:
-			self.SLAVE_X_still = 0
-			self.SLAVE_Y_still = self.all_axis[self.SLAVE_Y]
-			
+	def reset_still(self, x_button, y_button, x_still, y_still):
+		if self.all_axis[x_button]  and  self.all_axis[y_button]: 
+			x_still = self.all_axis[x_button]
+			y_still = self.all_axis[y_button]
+		elif self.all_axis[x_button] and not self.all_axis[y_button]:
+			x_still = self.all_axis[x_button]
+			y_still = 0
+		elif not self.all_axis[x_button] and self.all_axis[y_button]:
+			x_still = 0
+			y_still = self.all_axis[y_button]
+		return x_still, y_still
+		
+	def local_spider_move(self, rect, speed):
+		self.SLAVE_X_still, self.SLAVE_Y_still = self.reset_still(
+			self.SLAVE_X, self.SLAVE_Y, self.SLAVE_X_still, self.SLAVE_Y_still)
 		new_rect = rect.move(
 			self.SLAVE_X_still * speed, self.SLAVE_Y_still * speed )
 		return new_rect
