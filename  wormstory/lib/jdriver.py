0397ca6b7d2a61a00cc3757bf310eb958ae522f2
milk2cows <milk2cows@b8d21008-e54a-0410-bd0d-c175464d291e>
2008-04-23 08:52:07 +0000

    spider can autorun.

---  wormstory/lib/jdriver.py
+++  wormstory/lib/jdriver.py
@@ -3,13 +3,28 @@ from jioe import jioe
 
 class jdriver(jioe):
 	def __init__(self):
-		self.SLAVE_Y = 4
-		self.SLAVE_X = 7
-		self.MASTER_X = 1
-		self.MASTER_Y = 0
+		self.SLAVE_Y = 7
+		self.SLAVE_X = 4
+		self.MASTER_X = 0
+		self.MASTER_Y = 1
+		
+		# add a still logic
+		self.SLAVE_Y_still = 0
+		self.SLAVE_X_still = 0
+		
 		jioe.__init__(self) 
 		
-	def local_spider_move(self, rect, speed):
+	def local_spider_move(self, rect, speed):		
+		if self.all_axis[self.SLAVE_X]  and  self.all_axis[self.SLAVE_Y]: 
+			self.SLAVE_X_still = self.all_axis[self.SLAVE_X]
+			self.SLAVE_Y_still = self.all_axis[self.SLAVE_Y]
+		elif self.all_axis[self.SLAVE_X] and not self.all_axis[self.SLAVE_Y]:
+			self.SLAVE_X_still = self.all_axis[self.SLAVE_X]
+			self.SLAVE_Y_still = 0
+		elif not self.all_axis[self.SLAVE_X] and self.all_axis[self.SLAVE_Y]:
+			self.SLAVE_X_still = 0
+			self.SLAVE_Y_still = self.all_axis[self.SLAVE_Y]
+			
 		new_rect = rect.move(
-			self.all_axis[self.SLAVE_X] * speed, self.all_axis[self.SLAVE_Y] * speed )
+			self.SLAVE_X_still * speed, self.SLAVE_Y_still * speed )
 		return new_rect
