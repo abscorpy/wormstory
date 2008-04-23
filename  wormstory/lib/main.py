6cfcd70c86d8139695ba749cfe05b4fd144582b2
milk2cows <milk2cows@b8d21008-e54a-0410-bd0d-c175464d291e>
2008-04-23 17:06:29 +0000

    move debug handler under gobj.

---  wormstory/lib/main.py
+++  wormstory/lib/main.py
@@ -7,28 +7,27 @@ from game_debug import game_debug
 
 if __name__ == "__main__":
 	g = gobj()
-	gdebug = game_debug(g)
+#	gdebug = game_debug(g)
 	s = scene(g)
 	s.draw()
 	display.flip()
-	spider = spider(g, (100,300))
+#	spider = spider(g, (100,300))
 #	g.spider_group.add(spider)
 
 	while not g.j.quit and not g.j.all_buttons[9]['down'] and not g.j.all_buttons[21]['down']:
 		g.event_update()
-		spider.update()
+		g.slave_spider.update()
 #		g.spider_group.update()
 		s.draw()
-		g.screen.blit(spider.image.convert_alpha(), (spider.rect.left, spider.rect.top))
+		g.screen.blit(g.slave_spider.image.convert_alpha(), 
+			(g.slave_spider.rect.left, g.slave_spider.rect.top))
 
-		draw.line(g.screen, (255, 255, 255), 
-			(spider.rect.left, spider.rect.top - 1), 
-			(spider.rect.left + g.j.all_buttons[18]['pressed_time'] * 5, spider.rect.top - 5), 3)
-
+		g.debug.debug_pressed_time()
+		
 #		print 'top: %s' %spider.rect.top
 #		print 'left: %s' %spider.rect.left
 #		g.spider_group.draw(g.screen)
-#		gdebug.debug_jioe()
+		g.debug.debug_jioe()
 		display.flip()
 
 
