dacc9e06fd2549558093b520fa19647e8a671896
milk2cows <milk2cows@b8d21008-e54a-0410-bd0d-c175464d291e>
2008-04-30 08:43:47 +0000

    simple game Obj.

--- trunk/ wormstory/lib/gobj.py
+++ trunk/ wormstory/lib/gobj.py
@@ -3,8 +3,6 @@
 from game_utils import game_utils
 from pygame import *
 from jdriver import jdriver
-from spider import spider
-from game_debug import game_debug
 
 class gobj(object):
 	def __init__(self):
@@ -20,20 +18,14 @@ class gobj(object):
 		self.screen = display.set_mode(self.screen_size)
 		self.main_title = 'Worm Jumping'
 		self.set_display()
-		
-		self.game_intro_image = self.u.load_image('block.png')
+
 		self.playarea_image = self.u.load_image('S-3-800x600.png')
-#		self.playinfo_image = self.u.load_image('h12-200x600.png')
 		self.playinfo_image = self.u.load_image('S-3-200x600.png')
-		self.red_block_image = self.u.load_image('rsblock.png')
-		self.blue_block_image = self.u.load_image('bsblock.png')
-		self.green_block_image = self.u.load_image('gsblock.png')
 		self.dancing_block_image = self.u.load_image('dancingblock.png', -1)
-		self.spider_image = self.u.load_image('spider_s.png', -1)
 		self.knock_sounds = [
-			self.u.load_sound('25305__freesound__Human_Bomb_ses1.ogg'),
-			self.u.load_sound('32841__ERH__32_shot_b.ogg'),
-			self.u.load_sound('36846__EcoDTR__LaserRocket.ogg')
+			self.u.load_sound('humanbomb.ogg'),
+			self.u.load_sound('shotb.ogg'),
+			self.u.load_sound('laserrocket.ogg')
 			]
 
 		self.j = jdriver()
@@ -44,14 +36,12 @@ class gobj(object):
 		self.spider_group = sprite.Group()
 		self.block_group = sprite.Group()
 		self.groundblock_group = sprite.Group()
-		self.slave_spider = spider(self, (100,300))
-		self.debug = game_debug(self)
+#		self.debug = game_debug(self)
 		self.score = 100
 		self.hit_block = 0
 		self.game_more = 2
 		self.game_speed = 100
-
-		
+	
 	def event_update(self):
 		events = event.get()
 		self.j.update(events)
@@ -61,8 +51,4 @@ class gobj(object):
 #		display.toggle_fullscreen()
 
 if __name__ == "__main__":
-	
-#	screen = display.set_mode((800, 600))
-#	pygame.display.set_caption('Warm Story')
-#	pygame.mouse.set_visible(0)
 	g = gobj()
