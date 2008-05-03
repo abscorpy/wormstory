51c203285b9f8ca3c65b0a93f4de69325cfced80
milk2cows <milk2cows@b8d21008-e54a-0410-bd0d-c175464d291e>
2008-05-03 17:25:41 +0000

    move menus to playinfo_s, and change g.m_dict to g.m_list data type.

--- trunk/ wormstory/lib/menu.py
+++ trunk/ wormstory/lib/menu.py
@@ -8,8 +8,8 @@ class menu(object):
 	def set_menu_group(self):
 		self.g.menu_group.empty()
 		self._add_item()
-		for m in self.g.m_dict.keys():
-			self.g.menu_group.add(self.g.m_dict[m])
+		for m in self.g.m_list:
+			self.g.menu_group.add(m)
 
 	def _add_item(self):
 		pass
@@ -17,15 +17,15 @@ class menu(object):
 	def update(self):
 		# Is it a good idea. everytime to recreate the menubar ? 
 		self.set_menu_group()
-		if self.g.menu_choice_id < len(self.g.menu_group.sprites()):
+		if self.g.menu_choice_id < (len(self.g.menu_group.sprites()) - 1):
 			if self.g.d.io.all_buttons[6]['down']:
 				self.g.menu_choice_id += 1
-		if self.g.menu_choice_id > 1:
+		if self.g.menu_choice_id > 0:
 			if self.g.d.io.all_buttons[1]['down']:
 				self.g.menu_choice_id -= 1
 				
 		self.g.menu_group.update()
 		self.g.menu_group.draw(self.g.s.menu_s)
-		self.g.s.menu_s.blit(
+		self.g.s.playarea_s.blit(
 			self.g.game_info_small_font.render('http://www.milk2cows.com/dancingblock', 1,
-				(0,0,128)), (40,320))
+				(0,0,128)), (40,570))
