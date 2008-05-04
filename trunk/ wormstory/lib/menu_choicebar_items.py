d539ba3d2bc8c7e93f03dece5082f5ec3f5b2b29
milk2cows <milk2cows@b8d21008-e54a-0410-bd0d-c175464d291e>
2008-05-04 17:41:31 +0000

    try to write menu_choicebar_items.

--- trunk/ wormstory/lib/menu_choicebar_items.py
+++ trunk/ wormstory/lib/menu_choicebar_items.py
@@ -1,41 +1,14 @@
-# Hi I am menu_items.
+# Hi I am menu_choicebar_item.
 
 from pygame import *
+from menu_item import menu_item
 
-class menu_item(sprite.Sprite):
-	def __init__(self, g, title, location, gotopage):
-		sprite.Sprite.__init__(self)
-		self.g = g
-		self.title = title
-		self.gotopage = gotopage
-		self.non_active_color = (0,100,0)
-		self.active_color = (0, 150, 0)
-		self.message_title(self.non_active_color)
-		self.rect = self.image.get_rect(topleft=location)
-	def message_title(self, color):
-		self.image = self.g.menu_info_font.render(self.title, 1, color)
-	def update(self):
-		if self.g.m_list[self.g.menu_choice_id] == self:
-			self.message_title(self.active_color)
-		else:
-			self.message_title(self.non_active_color)
-
-class menu_play(menu_item):
-	def __init__(self, g, gotopage, location):
-		menu_item.__init__(self, g, 'PLAY', location, gotopage)
-
-class menu_configure(menu_item):
-	def __init__(self, g, gotopage, location):
-		menu_item.__init__(self, g, 'CONFIGURE', location, gotopage)
-
-class menu_quit(menu_item):
-	def __init__(self, g, gotopage, location):
-		menu_item.__init__(self, g, 'QUIT', location, gotopage)
-		
-class menu_back(menu_item):
-	def __init__(self, g, gotopage, location):
-		menu_item.__init__(self, g, 'BACK', location, gotopage)
-		
-class menu_main(menu_item):
-	def __init__(self, g, gotopage, location):
-		menu_item.__init__(self, g, 'MAIN', location, gotopage)
+class menu_choicebar_item(menu_item):
+	def __init__(self, g, title, location, valueobj):
+		self.valueobj = valueobj
+		menu_item.__init__(self, g, title, location)
+	def __set_mix_set_min(self, mix, min):
+		self.mix = mix
+		self.min = min
+	def __update_addon(self):
+		pass
