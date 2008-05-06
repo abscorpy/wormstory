737de40ed318bfcc1ed6a9b7272e91cf0b8f8280
milk2cows <milk2cows@b8d21008-e54a-0410-bd0d-c175464d291e>
2008-05-06 15:47:36 +0000

    can active the configure_menu and bar. but discover the old bug. ( all recreate in menu items ), miss a file.

---
+++  wormstory/lib/configure_menu_items.py
@@ -0,0 +1,33 @@
+# Hi I am configure_menu_items
+
+from menu_item import menu_item
+from choicebar import choicebar
+
+class choicebar_menu_item(menu_item, choicebar):
+	def __init__(self, g, title, pos, s, config_obj_value):
+		menu_item.__init__(self, g, title, pos)
+		choicebar.__init__(self, g, s, (pos[0] + 200, pos[1]), config_obj_value)
+	def _set_configobj_value(self, v):
+		objvalue = v
+	def update(self):
+		menu_item.update(self)
+		if self.active:
+			choicebar.update(self)
+	
+class speed_item(choicebar_menu_item):
+	def __init__(self, g, pos, s):
+		choicebar_menu_item.__init__(self, g, 'SPEED', pos, s, g.config_speed)
+	def set_configobj_value(self, v):
+		self.g.config_speed = v
+		
+class max_item(choicebar_menu_item):
+	def __init__(self, g, pos, s):
+		choicebar_menu_item.__init__(self, g, 'MAX', pos, s, g.config_max)
+	def set_configobj_value(self, v):
+		self.g.config_max = v
+	
+class keep_item(choicebar_menu_item):
+	def __init__(self, g, pos, s):
+		choicebar_menu_item.__init__(self, g, 'KEEP', pos, s, g.config_keep)
+	def set_configobj_value(self, v):
+		self.g.config_keep = v
