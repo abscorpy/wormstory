aa5855f84e1dd964cb26d1ff70fe4bd1aee52d9d
milk2cows <milk2cows@b8d21008-e54a-0410-bd0d-c175464d291e>
2008-05-06 03:25:16 +0000

    add the configure_game_menu , [ skip ] handler in page obj.

--- trunk/ wormstory/lib/page.py
+++ trunk/ wormstory/lib/page.py
@@ -5,28 +5,50 @@ class page(object):
 	def __init__(self, g):
 		self.g = g
 		self.clock = time.Clock()
-		self.mark_time = 2
 		self.playing_configure_state = 0
 		
+	def _skip_handle(self):
+			# handle skip.
+		self.g.screen.blit(
+			self.g.menu_info_font.render('[ SKIP ]', 1, (0, 150, 0)), (330, 550)
+			)
+		if self.g.d.io.all_buttons[8]['down']:
+			self.g.d.io.all_buttons[8]['down'] = 0
+			return self.g.MAIN_PAGE
+		else:
+			return 0
+
 	def mark_page(self):
+		self.mark_time = 30
 		while self.mark_time:
-			self.clock.tick(1)
+			self.clock.tick(10)
+			self.g.event_update()
 			self.mark_time -= 1
 			self.g.screen.blit(
 				self.g.cow_logo_image.convert_alpha(), (250,100)
 				)
+			
+			r = self._skip_handle()
+			if r: return r
+
 			display.flip()
-		self.mark_time = 2
+		self.mark_time = 30
 		while self.mark_time:	
-			self.clock.tick(1)
+			self.clock.tick(10)
+			self.g.event_update()
 			self.mark_time -= 1
 			self.g.screen.blit(
 				self.g.game_info_font.render('Milker\'s Solo Game', 1, 
 						(100,100,100)), (200,500))
+			
+			r = self._skip_handle()
+			if r: return r
+			
 			display.flip()
-		self.mark_time = 2
+		self.mark_time = 30
 		while self.mark_time:	
-			self.clock.tick(1)
+			self.clock.tick(10)
+			self.g.event_update()
 			self.mark_time -= 1
 			self.g.screen.blit(self.g.s.black_s, (0,0))
 			self.g.screen.blit(
@@ -38,10 +60,15 @@ class page(object):
 			self.g.screen.blit(
 				self.g.game_info_font.render('2008-4', 1, 
 						(100,100,100)), (200,420))
+			
+			r = self._skip_handle()
+			if r: return r
+			
 			display.flip()
 
 	def main_page(self):
 		#title page:
+		self.g.menu_choice_id = 0	
 		while not self.g.d.io.quit : # and not self.g.d.io.all_buttons[8]['down']:
 			self.clock.tick(10)
 			self.g.event_update()
@@ -62,30 +89,12 @@ class page(object):
 			if self.g.d.io.all_buttons[8]['down']:
 				self.g.d.io.all_buttons[8]['down'] = 0
 				return self.g.m_list[self.g.menu_choice_id].gotopage
-						
-#			if joystick.get_count():
-#				self.g.s.playarea_s.blit(
-#					self.g.game_info_font.render('PRESS [select] TO PLAY', 1, 
-#						(0,128,0)), (40,500))
-#				self.g.s.playarea_s.blit(
-#					self.g.game_info_small_font.render('^^ USE dance-pad TO PLAY', 1, 
-#						(0,0,128)), (40,550))
-#			else:
-#				self.g.s.playarea_s.blit(
-#					self.g.game_info_font.render('PRESS [SPACE] TO PLAY', 1, 
-#						(0,128,0)), (40,500))
-#				self.g.s.playarea_s.blit(
-#					self.g.game_info_small_font.render('^^ USE KEYBROAD TO PLAY', 1, 
-#						(0,0,128)), (40,550))
 
 			display.flip()
-			
-	def __check_playing_configure_state(self, t):
-		if self.g.d.io.all_buttons[8]['down']:
-			self.g.d.io.all_buttons[8]['down'] = 0
-			self.playing_configure_state = t
-					
+								
 	def game_page(self):
+		menu_state = 0
+		self.g.menu_choice_id = 0
 		self.mark_time = 72
 		while self.mark_time : # and not self.g.d.io.all_buttons[9]['down'] : #21
 			self.clock.tick(24)
@@ -94,10 +103,13 @@ class page(object):
 			# draw the background.
 			self.g.s.draw()
 
-			if self.g.score > 0 and not self.playing_configure_state:
+			if self.g.score > 0 and not menu_state:
 				self.g.groundblock_group.update()
 				self.g.groundblock_group.draw(self.g.s.playarea_s)
-				self.__check_playing_configure_state(1)
+				
+				if self.g.d.io.all_buttons[8]['down']:
+					self.g.d.io.all_buttons[8]['down'] = 0
+					menu_state = 1
 			elif self.g.score <= 0:
 				self.g.s.playarea_s.blit(
 					self.g.game_info_font.render('GAMEOVER', 1, (180,0,0)), (210,210))
@@ -105,7 +117,7 @@ class page(object):
 					self.g.game_info_small_font.render('http://www.milk2cows.com/dancingblock', 1,
 				(0,0,128)), (40,570))
 				self.mark_time -= 1
-			elif self.playing_configure_state:
+			elif menu_state:
 				self.g.s.playarea_s.blit(self.g.menu_background_image , (211, 201))
 				self.g.p_m.update()
 				self.g.s.playarea_s.blit(self.g.s.menu_s, (211, 201))
@@ -113,9 +125,11 @@ class page(object):
 #				self.__check_playing_configure_state(0)
 				if self.g.d.io.all_buttons[8]['down']:
 					self.g.d.io.all_buttons[8]['down'] = 0
-					self.playing_configure_state = 0
-					if not self.g.m_list[self.g.menu_choice_id].gotopage == self.g.GAME_PAGE:
-						return self.g.m_list[self.g.menu_choice_id].gotopage
+					menu_state = 0
+					if self.g.m_list[self.g.menu_choice_id].gotopage:
+						gotopage = self.g.m_list[self.g.menu_choice_id].gotopage
+						self.g.menu_choice_id = 0
+						return gotopage
 			
 			self.g.s.playinfo_s.blit(
 					self.g.game_info_font.render('s: %s' %self.g.score, 1,
@@ -130,21 +144,27 @@ class page(object):
 		return self.g.QUIT_PAGE
 	
 	def quit_page(self):
-		self.mark_time = 3
+		self.mark_time = 30
 		while self.mark_time:
-			self.clock.tick(1)
+			self.clock.tick(10)
+			self.g.event_update()
 			self.mark_time -= 1
 			self.g.screen.blit(self.g.s.black_s, (0,0))
 			self.g.screen.blit(
 				self.g.game_info_font.render('Thank You.', 1, 
-						(100,100,100)),(200,340))
+						(128,128,0)),(200,340))
 			self.g.screen.blit(
 				self.g.game_info_font.render('for you play this game.', 1,
-						(100,100,100)), (200,380))
+						(128,128,0)), (200,380))
+						
+			r = self._skip_handle()
+			if r: return r
+			
 			display.flip()
-		self.mark_time = 3
+		self.mark_time = 30
 		while self.mark_time:
-			self.clock.tick(1)
+			self.clock.tick(10)
+			self.g.event_update()
 			self.mark_time -= 1
 			self.g.screen.blit(self.g.s.black_s, (0,0))
 			self.g.screen.blit(
@@ -153,10 +173,15 @@ class page(object):
 			self.g.screen.blit(
 				self.g.game_info_font.render('bye !', 1,
 						(100,100,100)), (200,340))
+			
+			r = self._skip_handle()
+			if r: return r
+			
 			display.flip()
 	
 	def configure_page(self):
-#		print 'CP Out!!'
+		menu_state = 0
+		self.g.menu_choice_id = 0
 		while not self.g.d.io.quit :
 			self.clock.tick(10)
 			self.g.event_update()
@@ -165,19 +190,29 @@ class page(object):
 			# black the playarea_s
 			self.g.s.playarea_s.blit(self.g.s.black_s, (0,0))
 
-			# clear the menu_s.
-			self.g.s.menu_s.blit(self.g.s.black_s, (0,0))
-			self.g.c_m.update()
-			self.g.s.playinfo_s.blit(self.g.s.menu_s, (0, 420))
+			if not menu_state:
+				self.g.s.menu_choicebar_s.blit(self.g.s.black_s, (0,0))
+				self.g.c_g_m.update()
+				self.g.s.playarea_s.blit(self.g.s.menu_choicebar_s, (0,250))
+				
+				if self.g.d.io.all_buttons[8]['down']:
+					self.g.d.io.all_buttons[8]['down'] = 0
+					menu_state = 1
+					self.g.menu_choice_id = 0
 			
-#			self.g.s.menu_choicebar_s.blit(self.g.s.black_s, (0,0))
-#			self.g.c_b_m.update()
-#			self.g.s.playarea_s.blit(self.g.s.menu_choicebar_s, (0, 250))
-		
-			# Oh! this isn't my taste.
-			if self.g.d.io.all_buttons[8]['down']:
-				self.g.d.io.all_buttons[8]['down'] = 0
-#				print 'mcid: %s' %self.g.menu_choice_id
-				return self.g.m_list[self.g.menu_choice_id].gotopage
+			else:
+				# clear the menu_s.
+				self.g.s.menu_s.blit(self.g.s.black_s, (0,0))
+				self.g.c_m.update()
+				self.g.s.playinfo_s.blit(self.g.s.menu_s, (0, 420))
+				
+				if self.g.d.io.all_buttons[8]['down']:
+					self.g.d.io.all_buttons[8]['down'] = 0
+					menu_state = 0
+					
+					if self.g.m_list[self.g.menu_choice_id].gotopage:
+						gotopage = self.g.m_list[self.g.menu_choice_id].gotopage
+						self.g.menu_choice_id = 0
+						return gotopage
 			
 			display.flip()
